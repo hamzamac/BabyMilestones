fa24173632f1b8b941a5f589f28562d2aa53b35e
Makame Makame <hamzamac@live.com>
2018-07-24 14:20:51 +0200

    created baby class

---
+++ src/main/java/com/makame/diary/entity/Baby.java
@@ -0,0 +1,37 @@
+package com.makame.diary.entity;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Baby{
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long Id;
+    @Getter
+    private String firstName;
+    @Getter
+    private String lastName;
+
+    @OneToMany
+    private Set<FirstAction> firstActions = new HashSet<>();
+
+    public Baby(String firstName,String lastName ) {
+        
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    }
+
+    public Baby() {   }
+}
---
+++ src/main/java/com/makame/diary/entity/FirstAction.java
@@ -0,0 +1,5 @@
+package com.makame.diary.entity;
+
+public class FirstAction{
+    
+}
